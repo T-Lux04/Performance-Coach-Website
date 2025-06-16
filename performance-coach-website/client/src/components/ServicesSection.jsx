0f568524b2bd69385a4fbfdb713a6da7b3722de3
timothee lux <luxtimothee@gmail.com>
2025-06-16 13:41:15 +0100

    Added new service with image and links

--- performance-coach-website/client/src/components/ServicesSection.jsx
+++ performance-coach-website/client/src/components/ServicesSection.jsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import service1 from '../assets/images/service-1.png';
 import service2 from '../assets/images/service-2.png';
+import service3 from '../assets/images/service-3.png';
+
 import { Link } from "react-router-dom";
 
 const Button = ({ children, ...props }) => (
@@ -8,20 +10,21 @@ const Button = ({ children, ...props }) => (
   );
 
 export default function ServicesSection(){
-    const serviceImages = [service1, service2];
-    const serviceLink = ["/Booking", "/Contact"];
+    const serviceImages = [service1, service2, service3];
+    const serviceDescription = ["Our 1-1 coaching sessions are designed to elevate your game, refine your technique, and sharpen your mindset.","We offer elite-level restringing services tailored for athletes who demand precision, feel, and consistency." ,"A 1-to-1 performance session where we assess 5 key footwork patterns that define your game."]
+    const serviceLink = ["/Booking", "/Contact", "/Booking"];
 
     return(
         <>
         {/* Services Section */}
         <section id="services" className="py-20 px-10">
             <h2 className="text-4xl font-bold text-center emph">What I Offer</h2>
-            <div className="grid md:grid-cols-2 gap-8 mt-12">
-            {["1-on-1 Coaching", "Restringing Service"].map((service, index) => (
+            <div className="grid md:grid-cols-2 gap-8 justify-center mt-12">
+            {["1-on-1 Coaching", "Restringing Service", "Phase Movement IQ Test"].map((service, index) => (
                 <motion.div key={index} id="services" className="p-8 bg-black shadow-lg rounded-xl text-center border border-gold-500" whileHover={{ scale: 1.05, boxShadow: "0px 0px 20px rgba(255, 215, 0, 0.7)" }}>
                 <img src={serviceImages[index]} alt={service} className="rounded-lg mb-4 w-full h-80 object-contain" />
                 <h3 className="text-2xl font-semibold text-gold-400">{service}</h3>
-                <p className="mt-2 text-gray-400">A short description of the service.</p>
+                <p className="mt-2 text-gray-400">{serviceDescription[index]}</p>
                 <Link to={serviceLink[index]}>
                     <Button className="mt-6 bg-gold-400 text-black px-6 py-3 rounded-lg shadow-lg text-lg font-bold tracking-wide hover:bg-gold-500 transition-transform transform hover:scale-105">
                         Learn More

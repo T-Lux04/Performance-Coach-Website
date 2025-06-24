29926b4189730eaeaa1a3a49ebc48f7807606df8
timothee lux <luxtimothee@gmail.com>
2025-06-24 18:34:46 +0100

    altered the tension input type to allow a more dynamic choice

--- performance-coach-website/client/src/components/QuoteForm.jsx
+++ performance-coach-website/client/src/components/QuoteForm.jsx
@@ -110,7 +110,7 @@ function QuoteForm() {
           <ChevronDownIcon className="absolute top-12 right-3 h-5 w-5 pointer-events-none" />
         </div>
         <div className="relative">
-          <label htmlFor="tension" className="block text-lg font-semibold mb-2 emph">String Tension</label>
+          {/* <label htmlFor="tension" className="block text-lg font-semibold mb-2 emph">String Tension</label>
           <select
             name="tension"
             value={formData.tension}
@@ -122,7 +122,22 @@ function QuoteForm() {
             <option>50</option>
             <option>40</option>
           </select>
-          <ChevronDownIcon className="absolute top-12 right-3 h-5 w-5 pointer-events-none" />
+          <ChevronDownIcon className="absolute top-12 right-3 h-5 w-5 pointer-events-none" /> */}
+          
+          <label htmlFor="tension" className="block text-lg font-semibold mb-2 emph">String Tension (lbs)</label>
+          <input
+            type="number"
+            name="tension"
+            min="40"
+            max="80"
+            step="1"
+            value={formData.tension}
+            onChange={handleChange}
+            className="w-full rounded-md bg-gray-900 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-yellow-500"
+            placeholder="Enter tension (e.g. 67)"
+            required
+          />
+
         </div>
         <div className="sm:col-span-2">
           <label htmlFor="description" className="block text-lg font-semibold mb-2 emph">Description</label>
--- performance-coach-website/server/server.js
+++ performance-coach-website/server/server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import rateLimit from 'express-rate-limit';
 import bodyParser from 'body-parser';
 import { Resend } from 'resend';
 import dotenv from 'dotenv';
@@ -16,22 +15,15 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
-const limiter = rateLimit({
-  windowMs: 60 * 1000, // 1 minute
-  max: 5, // limit each IP to 5 requests per minute
-  message: 'Too many requests, please try again later.',
-  standardHeaders: true,
-  legacyHeaders: false, 
-});
 
-
-app.post('/api/contact', limiter, async (req, res) => {
+app.post('/api/contact', async (req, res) => {
   const { name, email, phone, service, message } = req.body;
 
   try {
     await resend.emails.send({
       from: 'Coach@phasetennis.co.uk',
       to: ['Coach@phasetennis.com'],
+      to: ['luxtimothee@gmail.com'],
       replyTo: `${name} <${email}>`,
       subject: `Restringing Service Request`,
       html: `

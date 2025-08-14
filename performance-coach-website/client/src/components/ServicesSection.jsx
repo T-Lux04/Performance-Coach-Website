5e7d2f7357ac65a9302a335f73cf2e294fbab727
timothee lux <luxtimothee@gmail.com>
2025-08-14 22:11:38 +0100

    Remove IQ-Test service from website

--- performance-coach-website/client/src/components/ServicesSection.jsx
+++ performance-coach-website/client/src/components/ServicesSection.jsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import service1 from '../assets/images/service-1.webp';
 import service2 from '../assets/images/service-2.webp';
-import service3 from '../assets/images/service-3.webp';
 
 import { Link } from "react-router-dom";
 
@@ -10,10 +9,10 @@ const Button = ({ children, ...props }) => (
   );
 
 export default function ServicesSection(){
-    const serviceImages = [service1, service2, service3];
-    const serviceDescription = ["Our 1-1 coaching sessions are designed to elevate your game, refine your technique, and sharpen your mindset.","We offer elite-level restringing services tailored for athletes who demand precision, feel, and consistency." ,"A 1-to-1 performance session where we assess 5 key footwork patterns that define your game."]
-    const serviceLink = ["/booking?type=one-on-one", "/Restringing", "/booking?type=iq-test"];
-    const linkDescription = ["Book a session at Phase Tennis Academy", "Learn more about restringing and our services", "Book an IQ Movement Test"]
+    const serviceImages = [service1, service2];
+    const serviceDescription = ["Our 1-1 coaching sessions are designed to elevate your game, refine your technique, and sharpen your mindset.", "We offer elite-level restringing services tailored for athletes who demand precision, feel, and consistency."];
+    const serviceLink = ["/booking?type=one-on-one", "/Restringing"];
+    const linkDescription = ["Book a session at Phase Tennis Academy", "Learn more about restringing and our services"]
 
     return(
         <>
@@ -21,7 +20,7 @@ export default function ServicesSection(){
         <section id="services" className="py-20 px-10">
             <h2 className="text-4xl font-bold text-center emph">What I Offer</h2>
             <div className="grid md:grid-cols-2 gap-8 justify-center mt-12">
-            {["1-on-1 Coaching", "Restringing Service", "Phase Movement IQ Test"].map((service, index) => (
+            {["1-on-1 Coaching", "Restringing Service"].map((service, index) => (
                 <motion.div key={index} id="services" className="p-8 bg-black shadow-lg rounded-xl text-center border border-gold-500" whileHover={{ scale: 1.05, boxShadow: "0px 0px 20px rgba(255, 215, 0, 0.7)" }}>
                 <div className="rounded-xl overflow-hidden mb-4 w-full h-80 flex items-center justify-center bg-black">
                     <img

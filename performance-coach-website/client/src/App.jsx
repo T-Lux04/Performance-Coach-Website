f946d8cf8b3a7ccd812e80e43971183c21929dec
timothee lux <luxtimothee@gmail.com>
2025-09-12 08:32:51 -1000

    reverting changes

--- performance-coach-website/client/src/App.jsx
+++ performance-coach-website/client/src/App.jsx
@@ -3,13 +3,15 @@ import Home from './pages/Home.jsx';
 import Restringing from './pages/Restringing.jsx';
 import Booking from './pages/Booking.jsx';
 
+
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/restringing" element={<Restringing />} />
-        <Route path="/booking" element={<Booking />} />
+  <Route path="/restringing" element={<Restringing />} />
+  <Route path="/booking" element={<Booking />} />
+
       </Routes>
     </Router>
   );
